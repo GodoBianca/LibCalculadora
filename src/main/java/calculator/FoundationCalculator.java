62449d7f99c7235761941ce00282605ecc379741
Bianca godo <bianca.godo@outlook.com>
2023-02-22 22:47:45 -0300

    primeiro commit

---
+++ src/main/java/calculator/FoundationCalculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class FoundationCalculator {
+    public int sumTwoNums(int a, int b) {
+        return a + b;
+    }
+    public int subtractTwoNums(int a, int b) {
+        return a - b;
+    }
+    public int divideTwoNums(int a, int b) {
+        return a / b;
+    }
+    public int multiplyTwoNums(int a, int b) {
+        return a * b;
+    }
+}
